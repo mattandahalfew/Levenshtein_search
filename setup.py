5956f15b581e9ade40b29051e055df7de61c187d
mattandahalfew <mattandahalfew@gmail.com>
2016-09-28 15:50:04 -0400

    Initial release of Levenshtein_search

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, Extension
+
+setup(
+name='Levenshtein_search',
+url='https://github.com/mattandahalfew/Levenshtein_search',
+version='1.0',
+author='Matt Anderson',
+author_email='manderson3606@gmail.com',
+description='Search through a dictionary to find words up to d distance away from a query word',
+packages=[],
+ext_modules=[Extension('Levenshtein_search',['Lev_search1_0.c'])],
+install_requires=[],
+classifiers=[
+	'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Scientific/Engineering',
+        'Topic :: Scientific/Engineering :: Information Analysis'])
